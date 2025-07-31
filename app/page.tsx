3e9e7e1533d0c9dfacf42aa31c5efc7490dbac01
Tomashevskaya Anastasia <128850738+Tomashevskaya-Anastasia@users.noreply.github.com>
2025-08-01 00:48:14 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import Footer from '@/components/Footer';
+import SideTabs from '@/components/SideTabs';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <SideTabs /> 
       <main> {/* чтоб не перекрыть хедером */}
         <Hero />
         {/* дальше добавишь <Features /> и другие секции */}
